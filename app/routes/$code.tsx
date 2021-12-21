12163cb1a0ea5fd24dd14d1e0bf7104ef811fef5
Siddharth Borderwala <54456279+siddharthborderwala@users.noreply.github.com>
2021-12-21 17:47:26 +0530

    add card

--- app/routes/$code.tsx
+++ app/routes/$code.tsx
@@ -1,34 +1,34 @@
-import { json, LoaderFunction, MetaFunction, useLoaderData } from 'remix'
-import { DBType } from '~/types'
-import { getGradientFromCode } from '~/util'
-import database from '../db.json'
+import { json, LoaderFunction, MetaFunction, useLoaderData } from 'remix';
+import { DBType } from '~/types';
+import { getGradientFromCode } from '~/util';
+import database from '../db.json';
 
-const db = database as DBType
+const db = database as DBType;
 
 export let loader: LoaderFunction = ({ params }) => {
-  const code = params.code as string
-  const res = db[code]
+  const code = params.code as string;
+  const res = db[code];
   if (!res) {
-    throw json({}, { status: 404 })
+    throw json({}, { status: 404 });
   }
-  return json({ code, ...res }, { status: 200 })
-}
+  return json({ code, ...res }, { status: 200 });
+};
 
 export let meta: MetaFunction = ({ params }) => {
-  const code = params.code as string
-  const res = db[code]
+  const code = params.code as string;
+  const res = db[code];
   return {
     title: `${code} - ${res.reason} | Http status codes for humans 😄`,
     description: `Learn more about ${code} http status code - ${res.reason}.`,
-  }
-}
+  };
+};
 
 export default function Index() {
   const { code, reason, brief, useCase } = useLoaderData<
     DBType & { code: string }
-  >()
+  >();
 
-  const gradient = getGradientFromCode(code)
+  const gradient = getGradientFromCode(code);
 
   return (
     <article>
@@ -43,6 +43,25 @@ export default function Index() {
       <p className="text-lg mt-6">{brief}</p>
       <h3 className="text-xl font-bold mt-6">Use Case</h3>
       <p className="text-lg mt-2">{useCase}</p>
+      <div className="rounded-md bg-gray-300 text-gray-700 p-8">
+        <h4 className="text-xl font-bold">Found a problem with this page?</h4>
+        <a
+          href="http://github.com/siddharthborderwala/httpcode"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="outline-none focus:ring text-lg text-indigo-500 cursor-pointer mt-6"
+        >
+          Edit this page on GitHub.
+        </a>
+        <a
+          href="http://github.com/siddharthborderwala/httpcode/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="outline-none focus:ring text-lg text-indigo-500 cursor-pointer mt-4"
+        >
+          Report a problem with this page.
+        </a>
+      </div>
     </article>
-  )
+  );
 }
